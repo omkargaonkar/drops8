0dfee34a9e695a33dfaea18eec9d22c3a2c718f4
Tanmay Khedekar <tanmay@osseed.com>
2018-01-11 13:04:00 +0530

    [#154107585] - Added comments and coding standard fixes.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -1,6 +1,11 @@
 <?php
 
 /**
+ * @file
+ * Main Drupal configurations file.
+ */
+
+/**
  * Load services definition file.
  */
 $settings['container_yamls'][] = __DIR__ . '/services.yml';
@@ -8,19 +13,22 @@ $settings['container_yamls'][] = __DIR__ . '/services.yml';
 /**
  * Include the Pantheon-specific settings file.
  *
- * n.b. The settings.pantheon.php file makes some changes
- *      that affect all envrionments that this site
- *      exists in.  Always include this file, even in
- *      a local development environment, to insure that
- *      the site settings remain consistent.
+ * The settings.pantheon.php file makes some changes that affect all
+ * envrionments that this site exists in.  Always include this file, even in
+ * a local development environment, to insure that the site settings remain
+ * consistent.
  */
 include __DIR__ . "/settings.pantheon.php";
 
 /**
- * If there is a local settings file, then include it
+ * If there is a local settings file, then include it.
  */
 $local_settings = __DIR__ . "/settings.local.php";
 if (file_exists($local_settings)) {
   include $local_settings;
 }
-$settings['install_profile'] = 'standard';
+
+/**
+ * Hard set installation profile name.
+ */
+$settings['install_profile'] = 'seed';
--- sites/default/template.settings.local.php
+++ sites/default/template.settings.local.php
@@ -1,11 +1,19 @@
 <?php
 
+/**
+ * @file
+ * Template for local configuration file.
+ */
+
+/**
+ * Default databse configurations.
+ */
 $databases['default']['default'] = array(
-    'database' => '',
-    'username' => '',
-    'password' => '',
-    'host' => 'localhost',
-    'driver' => 'mysql',
-    'port' => 3306,
-    'prefix' => '',
+  'database' => '????',
+  'username' => '????',
+  'password' => '????',
+  'host' => 'localhost',
+  'driver' => 'mysql',
+  'port' => 3306,
+  'prefix' => '',
 );
